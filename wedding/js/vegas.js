fb53f38667f9daa86557fb3ebf835ff251cc5787
celsom3 <celso.mireles@gmail.com>
2014-12-17 00:17:36 -0600

    Find a good theme and use that one as the default wedding theme.

---
+++ wedding/js/vegas.js
@@ -0,0 +1,17 @@
+$(function() {
+    $.vegas("slideshow", {
+        backgrounds: [{
+            src: "img/bg1.jpg",
+            fade: 1e3
+        }, {
+            src: "img/bg2.jpg",
+            fade: 1e3
+        }, {
+            src: "img/bg3.jpg",
+            fade: 1e3
+        }, {
+            src: "img/bg4.jpg",
+            fade: 1e3
+        }]
+    })
+});
