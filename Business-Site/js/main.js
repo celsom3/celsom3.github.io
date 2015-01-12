e7105b11ecfe09260585aee89ce643f3a7495cd7
celsom3 <celso.mireles@gmail.com>
2015-01-11 21:47:50 -0600

    Update content, start playing with skrollr.js

--- Business-Site/js/main.js
+++ Business-Site/js/main.js
@@ -1,1 +1,13 @@
+( function( $ ) {
+    // Init Skrollr
+    var s = skrollr.init({
+        render: function(data) {
+            //Debugging - Log the current scroll position.
+            //console.log(data.curTop);
+        }
+    });
+} )( jQuery );
+
+// Wait for document to load before loading scripts
+
 
