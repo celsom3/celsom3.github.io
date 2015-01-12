6043c69653220f9d030b4e9dfbd72a095e42958f
celsom3 <celso.mireles@gmail.com>
2015-01-12 17:44:58 -0600

    Start work on another business template.

---
+++ business2/js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+	$(".textroll9").textroll({
+                easing: 'easeOutBounce',
+                speed: 700,
+                delay: 400
+            });
+
+	$.syntax({
+        root: 'js/jquery-syntax/'
+    });
+});
