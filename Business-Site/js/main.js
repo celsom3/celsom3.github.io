6b146e4bddc0099240db80dac2701504bae6aeae
celsom3 <celso.mireles@gmail.com>
2015-01-12 01:51:11 -0600

    Add parallax effect to intro and contact form.

--- Business-Site/js/main.js
+++ Business-Site/js/main.js
@@ -1,12 +1,4 @@
-( function( $ ) {
-    // Init Skrollr
-    var s = skrollr.init({
-        render: function(data) {
-            //Debugging - Log the current scroll position.
-            //console.log(data.curTop);
-        }
-    });
-} )( jQuery );
+
 
 // Wait for document to load before loading scripts
 
