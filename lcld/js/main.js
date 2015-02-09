e9115538dbd3e5d9818640731be869f3c24e00f1
celsom3 <celso.mireles@gmail.com>
2015-02-08 20:24:43 -0700

    Add new LCLD splash concept

---
+++ lcld/js/main.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+	
+	// Sets the height of all sections to screen height
+
+	var screenHeight = $(window).height();
+
+	$('#construction').css('height', screenHeight);
+	$('#about').css('height', screenHeight);
+	$('#dates').css('height', screenHeight);
+
+	// Smooth Scrolling
+	$('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
