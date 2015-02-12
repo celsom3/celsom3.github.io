1355c6178fe7692fd7b1300d83c9740b51858358
celsom3 <celso.mireles@gmail.com>
2015-02-11 22:49:31 -0700

    LCLD: Add nav menu and animations.

--- lcld/js/main.js
+++ lcld/js/main.js
@@ -9,8 +9,23 @@ $(document).ready(function(){
 	$('#about').css('min-height', screenHeight);
 	$('#dates').css('min-height', screenHeight);
 
-	// Smooth Scrolling
-	$('a[href*=#]:not([href=#])').click(function() {
+  // Initializes Bigfoot footnotes plugin
+  $.bigfoot();
+
+  $('#menu').slicknav({
+    'label' : '', // Label for menu button. Use an empty string for no label.
+    'duplicate': true, // If true, the mobile menu is a copy of the original.
+    'duration': 800, // The duration of the sliding animation.
+    'easingOpen': 'easeOutBounce', // Easing used for open animations.
+    'easingClose': 'easeOutBounce', // Easing used for close animations.
+    'prependTo': '#floating-menu', // Element, jQuery object, or jQuery selector string to prepend the mobile menu to.
+    'parentTag': 'a', // Element type for parent menu items.
+    'closeOnClick': true // Close menu when a link is clicked.
+  });
+
+
+  // Smooth Scrolling
+  $('a[href*=#]:not([href=#]).nav-item').click(function() {
     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
       var target = $(this.hash);
       target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
@@ -22,7 +37,4 @@ $(document).ready(function(){
       }
     }
   });
-
-  // Initializes Bigfoot footnotes plugin
-  $.bigfoot();
 });
