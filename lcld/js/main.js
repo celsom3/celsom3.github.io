5f6ee274e2b458d319922d8dfbee96f2613d77de
celsom3 <celso.mireles@gmail.com>
2015-02-16 14:31:46 -0700

    LCLD: Add more friend page navigation

--- lcld/js/main.js
+++ lcld/js/main.js
@@ -24,8 +24,10 @@ $(document).ready(function(){
   });
 
 
-  // Smooth Scrolling
-  $('a[href*=#]:not([href=#]).nav-item').click(function() {
+  
+
+  // Smooth Scrolling a[href*=#]:not([href=#])   
+  $('.nav-item').click(function() {
     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
       var target = $(this.hash);
       target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
@@ -33,8 +35,57 @@ $(document).ready(function(){
         $('html,body').animate({
           scrollTop: target.offset().top
         }, 1000);
+
         return false;
       }
     }
+
   });
+
+  // Initialize the first #fixed-scroller to link to current section
+
+  var currentSection = $(document).closest('section').attr('id');
+  var currentSectionStr = '#' + currentSection;
+  $('#fixed-scroller').attr('href', "#about");
+
+  $(document).scroll(function(){
+
+    // Make the #fixed-scroller automatically scroll to the next div
+    // Get the current position/section and find the next section ID
+
+    var cutoff = $(window).scrollTop();
+    var nextSection;
+    $('section').each(function(){
+      if($(this).offset().top + $(this).height() > cutoff){
+          // Find the next section
+          var nextSection = $(this).closest('section').next().attr('id');
+          var nextSectionStr = '#' + nextSection;
+
+          // Change #fixed-scroller to next section
+          $('#fixed-scroller').attr('href', nextSectionStr);
+
+          return false; // stops the iteration after the first one on screen
+      }
+
+    });
+
+    // Checks if you are in the last section. If so, change #fixed-scroller
+    // link to the top section and make the arrow point up
+
+    while($('#fixed-scroller').attr('href') === '#footer'){
+      $('#fixed-scroller').attr('href', '#construction');
+      $('#fixed-scroller > i').removeClass('fa-arrow-circle-down').addClass('fa-arrow-circle-up');
+    }
+
+    // If you are back at the top, change the arrow to point down
+    
+    if($('#fixed-scroller').attr('href') === '#about'){
+      $('#fixed-scroller > i').removeClass('fa-arrow-circle-up').addClass('fa-arrow-circle-down');
+    }
+
+
+  });
+
 });
+
+
