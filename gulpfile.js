10ac559e75aeddd2c6e60e24cd83c20d0f66d1aa
Celso Mireles <celso@digitalstrategy.tips>
2016-06-25 00:08:33 -0700

    Clean up Gulp file.

--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,11 @@
-// Defining base pathes
-var basePaths = {
-    bower: './bower_components/',
-    dev: './src/'
-};
-
 // Defining requirements
 var gulp = require('gulp');
 var browserSync = require('browser-sync');
 var reload = browserSync.reload;
 var harp = require('harp');
-
-//var plumber = require('gulp-plumber');
-// var sass = require('gulp-sass');
 var watch = require('gulp-watch');
-// var cssnano = require('gulp-cssnano');
-//var rename = require('gulp-rename');
-var concat = require('gulp-concat');
-// var uglify = require('gulp-uglify');
-//var jade = require('gulp-jade');
 var del = require('del');
 var prettify = require('gulp-prettify');
-//var livereload = require('gulp-livereload');
-var plumber = require('gulp-plumber');
-var rimraf = require('gulp-rimraf');
 var clean = require('gulp-clean');
 
 var paths = {
@@ -78,27 +61,14 @@ gulp.task('compile', function() {
 	return harp.compile(paths.harp , paths.harpCompiled, function() {
 		// After compiling with harp, move to root directory
 		gulp.src('_harp/www/**/*')
-			.pipe(plumber())
 			.pipe(prettify())
 			.pipe(gulp.dest('./'));
-		// Now delete all files in /www
-		//del('_harp/www/**/*');
-		del.sync(['_harp/www']);
-
 	});
-
-	//del.sync(['_harp/www']);
-
-
 
 });
 
-gulp.task('clean', ['compile'], function() {
-	//return del.sync(['_harp/www/**/*']);
-
-
-	return gulp.src('_harp/www', {read: false})
-	 	.pipe(clean('*', {force:true}));
+gulp.task('clean', function() {
+	return del.sync(['_harp/www']);
 });
 
 /**
