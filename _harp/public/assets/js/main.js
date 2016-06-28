6e2a07d4d33f6bc6fef3c47e5c3fb4241cbea14c
Celso Mireles <celso@digitalstrategy.tips>
2016-06-28 15:26:57 -0700

    Adjust smooth scroll

--- _harp/public/assets/js/main.js
+++ _harp/public/assets/js/main.js
@@ -44,7 +44,7 @@ $(document).ready(function(){
 	$('body').on('click', 'a.page-scroll', function(event) {
         var $anchor = $(this);
         $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top
+            scrollTop: $($anchor.attr('href')).offset().top - 130
         }, 1500, 'easeInOutExpo');
         event.preventDefault();
     });
