66c340c35f1bdfa12985a438c56bacc4d4be1855
Celso Mireles <celso@digitalstrategy.tips>
2016-06-28 12:17:01 -0700

    Add jQuery easing scroll animations

--- _harp/public/assets/js/hovershow.js
+++ _harp/public/assets/js/hovershow.js
@@ -18,6 +18,8 @@
 	// In the future, this will be the default selector
 	var hoverShow_Class = '.hoverShow';
 
+	var currentQuadrant = null;
+
 
 	// Initialize a data object where translation will get appended to.
 	var opts = {}
@@ -27,6 +29,51 @@
 	hoverShow.prototype = {
 
 		listening: function() {
+
+		},
+
+		getMousePosition: function() {
+			var currentMousePos = { x: -1, y: -1 };
+		    $(document).mousemove(function(event) {
+		        currentMousePos.x = event.pageX;
+		        currentMousePos.y = event.pageY;
+						// console.log('X: ' + currentMousePos.x);
+						// console.log('Y: ' + currentMousePos.y);
+						// console.log(whichQuadrant(currentMousePos, getMiddlePoint()));
+		    });
+				return currentMousePos;
+		},
+		getMiddlePoint: function(){
+			var wHeight = $(window).height();
+			var wWidth = $(window).width();
+			return {
+				x: wWidth / 2,
+				y: wHeight / 2
+			};
+		},
+
+		whichQuadrant: function(pos, middle) {
+
+			// var pos = getMousePosition();
+			// var middle = getMiddlePoint();
+			var pos = pos;
+			var middle = middle;
+
+			var belowMiddle = pos.y > middle.y;
+			var toRight = pos.x > middle.x;
+			if(belowMiddle && toRight){
+
+				return 2;
+			} else if ( belowMiddle && !toRight ){
+				return 3;
+			} else if ( !belowMiddle && toRight) {
+				return 1;
+			} else if ( !belowMiddle && !toRight){
+				return 4;
+			} else {
+				return 1;
+			}
+
 
 		},
 
--- _harp/public/assets/js/main.js
+++ _harp/public/assets/js/main.js
@@ -51,6 +51,15 @@ $(document).ready(function(){
 	});
 
 
+	// Smooth scroll
+	$('body').on('click', 'a.page-scroll', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+
 
 //===============================
 //
